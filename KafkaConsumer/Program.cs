528c4a6ec92487e67a834e66494648a0c5568a85
First Wan <siewyik85@hotmail.com>
2025-05-20 20:37:21 +0800

    Initial commit

---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,41 @@
+﻿using Confluent.Kafka;
+
+Console.WriteLine("Consumer Starting");
+
+string topic = "order";
+string groupId = "kitchen-group-1";
+string bootstrapServer = "";
+string apiKey = "";
+string apiSecret = "";
+
+ConsumerConfig config = new()
+{
+    GroupId = groupId,
+    AutoOffsetReset = AutoOffsetReset.Earliest,
+    BootstrapServers = bootstrapServer,
+    SecurityProtocol = SecurityProtocol.SaslSsl,
+    SaslMechanism = SaslMechanism.Plain,
+    SaslUsername = apiKey,
+    SaslPassword = apiSecret,
+};
+
+// creates a new consumer instance
+using (var consumer = new ConsumerBuilder<string, string>(config).Build())
+{
+    consumer.Subscribe(topic);
+    while (true)
+    {
+        // consumes messages from the subscribed topic and prints them to the console
+        var cr = consumer.Consume();
+
+        try
+        {
+            Console.WriteLine($"Consumed event from topic \"{topic}\": value = {cr.Message.Value}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Kafka Practice
+This is a small project that integrated Apache Kafka that included 2 project. One to produce event message and another one subscribe and consume event message from Apache Kafka.
+
+More implementation can read HERE.
